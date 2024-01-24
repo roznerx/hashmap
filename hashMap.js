1f86f9600bd67f7a35ab68569bf95336110da852
unknown <ignaciorosner@gmail.com>
2024-01-24 18:39:13 -0300

    Assignment finished without including extras yet; also added some notes in order to refactor repeated code, and also console logs in order to test (which I didn't include in linked list project

--- hashMap.js
+++ hashMap.js
@@ -1,10 +1,13 @@
 class HashMap {
   constructor (initialCapacity, loadFactor) {
-    this.buckets = new Array(initialCapacity);
+    // This sets the buckets with empty arrays
+    this.buckets = new Array(initialCapacity).fill(null).map(() => []);
+    this.loadFactor = loadFactor;
     this.size = 0;
   }
 
   hash(value) {
+    // Same method as the one provided in the HashMap Lesson
     let hashCode = 0;
     let primeNumber = 31;
 
@@ -15,17 +18,21 @@ class HashMap {
     return hashCode;
   };
 
+  // Here I'll create a method for finding a bucketEntry
+  // Here, another for resizing
+
   set(key, value) {
-
-    if (!this.buckets) {
-      console.log("Buckets are not properly initialized.");
-      return;
-    };
-
+    // Could be own method
     let hashKey = this.hash(key);
     let bucketIndex = hashKey % this.buckets.length;
 
-    let bucketEntry = (this.buckets[bucketIndex]).find(entry => entry[0] === key);
+    if (!this.buckets[bucketIndex]) {
+      this.buckets[bucketIndex] = [];
+    };
+
+    let bucketEntry = (this.buckets[bucketIndex] || [])
+      .find(entry => entry && entry[0] === key);
+    //
 
     if (bucketEntry) {
       bucketEntry[1] = value;
@@ -34,13 +41,153 @@ class HashMap {
       this.size++;
     };
 
-    // Should add a condition for resize if loadFactor is reached
-    // Something like
-
+    // Resize (could be own method)
     if (this.size / this.buckets.length > this.loadFactor) {
-      //resize bucket?
-    }
+      let newCapacity = this.buckets.length * 2;
+      let newBuckets = new Array (newCapacity).fill(null).map(() => []);
 
+      this.buckets.forEach(b => {
+        b.forEach(be => {
+          let [key, value] = be;
+          let beIndex = this.hash(key) % newCapacity;
+          newBuckets[beIndex].push([key, value]);
+        })
+      });
+
+      this.buckets = newBuckets;
+    };
+  };
+
+  get(key) {
+    // Could be own method to avoid repeating
+    let hashKey = this.hash(key);
+    let bucketIndex = hashKey % this.buckets.length;
+
+    if (!this.buckets[bucketIndex]) {
+      this.buckets[bucketIndex] = [];
+    };
+    
+    let bucketEntry = (this.buckets[bucketIndex] || [])
+      .find(e => e && e[0] === key);
+    //
+    return bucketEntry ? bucketEntry[1] : null;
+  };
+
+  has(key) {
+    // Could be own method to avoid repeating
+    let hashKey = this.hash(key);
+    let bucketIndex = hashKey % this.buckets.length;
+
+    if (!this.buckets[bucketIndex]) {
+      this.buckets[bucketIndex] = [];
+    };
+    
+    let bucketEntry = (this.buckets[bucketIndex] || [])
+      .find(e => e && e[0] === key);
+    //
+    return bucketEntry ? true : false;
+  };
+
+  remove(key) {
+    // Could be own method to avoid repeating
+    let hashKey = this.hash(key);
+    let bucketIndex = hashKey % this.buckets.length;
+
+    if (!this.buckets[bucketIndex]) {
+      this.buckets[bucketIndex] = [];
+    };
+    
+    let bucketEntry = (this.buckets[bucketIndex] || [])
+      .find(e => e && e[0] === key);
+    //
+
+    if (!bucketEntry) {
+      return false;
+    } else {
+      this.buckets[bucketIndex].length = 0;
+      this.size--;
+    };
+  };
+
+  length() {
+    return this.size;
+  };
+
+  clear() {
+    this.buckets.forEach(b => {
+      b.length = 0;
+      this.size = 0;
+    });
+  };
+
+  keys() {
+    let keyArr = [];
+
+    this.buckets.forEach(b => {
+      b.forEach(e => {
+        keyArr.push(e[0]);
+      });
+    });
+
+    return keyArr;
+  };
+
+  values() {
+    let valueArr = [];
+
+    this.buckets.forEach(b => {
+      b.forEach(e => {
+        valueArr.push(e[1]);
+      });
+    });
+
+    return valueArr;
+  };
+
+  entries() {
+    let entriesArr = [];
+
+    this.buckets.forEach(b => {
+      b.forEach(e => {
+        entriesArr.push(e);
+      });
+    });
+
+    return entriesArr;
   };
 };
 
+const myHashMap = new HashMap(16, 0.75);
+myHashMap.set("name", "Jake");
+myHashMap.set("age", 34);
+myHashMap.set("city", "Nevada");
+myHashMap.set("country", "USA");
+myHashMap.set("occupation", "Developer");
+myHashMap.set("guitar", "Les Paul");
+myHashMap.set("bass", "Fender");
+myHashMap.set("drums", "Tama");
+myHashMap.set("faveBand", "Converge");
+myHashMap.set("faveFilm", "Point Break");
+myHashMap.set("faveBook", "The DaVinci Code");
+myHashMap.set("faveActor", "Paul Giamatti");
+myHashMap.set("faveActress", "Margot Robbie");
+myHashMap.set("celebCrush", "Hunter Schafer");
+myHashMap.set("faveVideoGame", "Doom");
+myHashMap.set("faveAlbum", "Toxicity");
+myHashMap.set("faveFood", "Pizza");
+myHashMap.set("faveTvShow", "True Detective");
+myHashMap.set("faveSport", "Basketball");
+myHashMap.set("faveTeam", "LA Clippers");
+myHashMap.set("favePlayer", "Allen Iverson");
+console.log(myHashMap);
+console.log(myHashMap.buckets.length);
+console.log(myHashMap.size);
+console.log(myHashMap.get("faveVideoGame"))
+myHashMap.remove("faveVideoGame");
+console.log(myHashMap);
+console.log(myHashMap.length());
+//myHashMap.clear();
+console.log(myHashMap);
+console.log(myHashMap.keys());
+console.log(myHashMap.values());
+console.log(myHashMap.entries());
